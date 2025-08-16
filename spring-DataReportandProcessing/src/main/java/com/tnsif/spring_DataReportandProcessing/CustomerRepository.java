94ac34030e3e6ef70156c44903e402c38810894f
monik <monik@DESKTOP-ITUTF8H>
2025-08-16 20:57:21 +0530

    SpringBootProject

---
+++ spring-DataReportandProcessing/src/main/java/com/tnsif/spring_DataReportandProcessing/CustomerRepository.java
@@ -0,0 +1,5 @@
+package com.tnsif.spring_DataReportandProcessing;
+import org.springframework.data.jpa.repository.JpaRepository;
+public interface CustomerRepository extends JpaRepository<Customer, Long>{
+
+}
---
+++ spring-DataReportandProcessing/src/main/java/com/tnsif/spring_DataReportandProcessing/OrderRepository.java
@@ -0,0 +1,5 @@
+package com.tnsif.spring_DataReportandProcessing;
+import org.springframework.data.jpa.repository.JpaRepository;
+public interface OrderRepository extends JpaRepository<Order, Long>{
+
+}
