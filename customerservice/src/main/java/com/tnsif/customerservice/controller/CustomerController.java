da2e6cbb32141052fa4fa65140f89e277e53da8d
Mathew <Mathew@DESKTOP-ITUTF8H>
2025-09-05 20:54:55 +0530

    SpringBoot Project

--- customerservice/src/main/java/com/tnsif/customerservice/controller/CustomerController.java
+++ customerservice/src/main/java/com/tnsif/customerservice/controller/CustomerController.java
@@ -1,4 +1,5 @@
 package com.tnsif.customerservice.controller;
+
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -16,45 +17,58 @@ import com.tnsif.customerservice.entity.Customer;
 import com.tnsif.customerservice.service.CustomerService;
 
 import jakarta.persistence.NoResultException;
+
 @RestController
 public class CustomerController {
-	@Autowired
-	private CustomerService c;
-	@GetMapping("/customerservice")
-	public List<Customer> list(){
-		return c.listAll();
-	}
-	
-	@PostMapping("/customerservice")
-	public void add(@RequestBody Customer c1) {
-		c.save(c1);
-	}
-	
-	@GetMapping("/customerservice/{id}")
-	public ResponseEntity<Customer> get(@PathVariable Integer id){
-		try {
-			Customer c2 = c.get(id);
-			return new ResponseEntity<Customer>(c2,HttpStatus.OK);
-		}catch(NoResultException e) {
-			return new ResponseEntity<Customer>(HttpStatus.NOT_FOUND);
-		}
-	}
-	
-	@DeleteMapping("/customerservice/{id}")
-	public void delete(@PathVariable Integer id) {
-		c.delete(id);
-	}
-	
-	@PutMapping("/customerservice/{id}")
-	public ResponseEntity<Customer> update(@PathVariable Integer id,@RequestBody Customer update_c){
-		try {
-			Customer exist_c = c.get(id);
-			exist_c.setC_name(update_c.getC_name());
-			exist_c.setAddress(update_c.getAddress());
-			c.update(exist_c);
-			return new ResponseEntity<Customer>(exist_c,HttpStatus.OK);
-		}catch(NoResultException e) {
-			return new ResponseEntity<Customer>(HttpStatus.NOT_FOUND);
-		}
-	}
+
+    @Autowired
+    private CustomerService c;
+
+    // Get all customers
+    @GetMapping("/customerservice")
+    public List<Customer> list() {
+        return c.listAll();
+    }
+
+    // Add a new customer (all 5 fields)
+    @PostMapping("/customerservice")
+    public void add(@RequestBody Customer c1) {
+        c.save(c1);
+    }
+
+    // Get customer by ID
+    @GetMapping("/customerservice/{id}")
+    public ResponseEntity<Customer> get(@PathVariable Integer id) {
+        try {
+            Customer c2 = c.get(id);
+            return new ResponseEntity<>(c2, HttpStatus.OK);
+        } catch (NoResultException e) {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+    }
+
+    // Delete customer by ID
+    @DeleteMapping("/customerservice/{id}")
+    public void delete(@PathVariable Integer id) {
+        c.delete(id);
+    }
+
+    // Update existing customer (all 5 fields)
+    @PutMapping("/customerservice/{id}")
+    public ResponseEntity<Customer> update(@PathVariable Integer id, @RequestBody Customer update_c) {
+        try {
+            Customer exist_c = c.get(id);
+
+            // Update all fields
+            exist_c.setC_name(update_c.getC_name());
+            exist_c.setAddress(update_c.getAddress());
+            exist_c.setEmail(update_c.getEmail());
+            exist_c.setPhone(update_c.getPhone());
+
+            c.update(exist_c);
+            return new ResponseEntity<>(exist_c, HttpStatus.OK);
+        } catch (NoResultException e) {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+    }
 }
--- customerservice/src/main/java/com/tnsif/customerservice/entity/Customer.java
+++ customerservice/src/main/java/com/tnsif/customerservice/entity/Customer.java
@@ -1,58 +1,87 @@
 package com.tnsif.customerservice.entity;
+
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
 
 @Entity
-
 public class Customer {
-	@Id
-	@Column(name = "C_id")
-	private int c_id;
-	@Column(name = "C_name")
-	private String c_name;
-	@Column(name = "Address")
-	private String address;
-	
-	public Customer() {
-		super();
-		// TODO Auto-generated constructor stub
-	}
 
-	public Customer(int c_id, String c_name, String address) {
-		super();
-		this.c_id = c_id;
-		this.c_name = c_name;
-		this.address = address;
-	}
+    @Id
+    @Column(name = "C_id")
+    private int c_id;
 
-	public int getC_id() {
-		return c_id;
-	}
+    @Column(name = "C_name")
+    private String c_name;
 
-	public void setC_id(int c_id) {
-		this.c_id = c_id;
-	}
+    @Column(name = "Address")
+    private String address;
 
-	public String getC_name() {
-		return c_name;
-	}
+    @Column(name = "Email")
+    private String email;
 
-	public void setC_name(String c_name) {
-		this.c_name = c_name;
-	}
+    @Column(name = "Phone")
+    private String phone;
 
-	public String getAddress() {
-		return address;
-	}
+    // Default constructor
+    public Customer() {
+        super();
+    }
 
-	public void setAddress(String address) {
-		this.address = address;
-	}
+    // Parameterized constructor (with 5 fields)
+    public Customer(int c_id, String c_name, String address, String email, String phone) {
+        super();
+        this.c_id = c_id;
+        this.c_name = c_name;
+        this.address = address;
+        this.email = email;
+        this.phone = phone;
+    }
 
-	@Override
-	public String toString() {
-		return "Customer [c_id=" + c_id + ", c_name=" + c_name + ", address=" + address + "]";
-	}
-	
+    // Getters and Setters
+    public int getC_id() {
+        return c_id;
+    }
+
+    public void setC_id(int c_id) {
+        this.c_id = c_id;
+    }
+
+    public String getC_name() {
+        return c_name;
+    }
+
+    public void setC_name(String c_name) {
+        this.c_name = c_name;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    @Override
+    public String toString() {
+        return "Customer [c_id=" + c_id + ", c_name=" + c_name + ", address=" + address 
+                + ", email=" + email + ", phone=" + phone + "]";
+    }
 }
